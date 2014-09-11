d06e4def5a5110cb8789388875980cde20012b2c
Serge Tsygankov <stsigankov@gmail.com>
2014-09-12 01:09:53 +0400

    cached property

---
+++ autofootball/langtools.py
@@ -0,0 +1,9 @@
+__author__ = 'baza'
+
+class cached_property(object):
+    def __init__(self, func):
+        self.func = func
+
+    def __get__(self, instance, cls=None):
+        result = instance.__dict__[self.func.__name__] = self.func(instance)
+        return result
